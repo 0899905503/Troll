ce5fc2173ea90119bd2e5c415818257cb48f8b6d
BAVO <kellyvo11309@gmail.com>
2023-02-24 21:42:21 +0700

    first commit

---
+++ MB/MB/Form1.cs
@@ -0,0 +1,23 @@
+﻿namespace MB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         DialogResult = MessageBox.Show("BAVO so handsome ?","Answer",MessageBoxButtons.YesNo);
+            if (DialogResult == DialogResult.Yes)
+            {
+                MessageBox.Show("Of Couse ! BAVO so handsome");
+            }
+            else
+            {
+                MessageBox.Show("Your answer is wrong ! BAVO so handsome is the right answer ");
+            }
+        }
+    }
+}
